dddd98fd5ab701b22dd31a1cae776486e7ce3dc3
TarikElBali <tarik_elbali@hotmail.de>
2026-01-12 09:43:26 +0000

    02 / Variablen vs Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -1,4 +1,4 @@
-// Zeilenkommentar  | Strg + #
+//  Zeilenkommentar  | Strg + #
 
 /**
  * 
@@ -10,14 +10,14 @@
 
 /***** Variablen 01 *******/
 
+//console.log("hi");
+//console.log("hi");
 
+/*Deklaration + Wertzuweisung I */
 
-/* Deklaration + Wertzuweisung I */
-
-
-
-
-
+//let firstName; // Deklaration (Definition)
+//console.log(firstname); // Ausgabe
+//firstName = ""
 
 
 /*  
@@ -27,9 +27,35 @@
 
 
 
+// let test; // Deklaration
 
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
+// test = "hi";
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+// test = true;
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
 
 
 /* Variablen vs. Konstanten in JS */
 
+// Variable
+let test;  // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "Hi!";
+// test2 = "hi"; // Laufzeitfehler!!
+console.log("Inhalt: " + test2); // Ausgabe
+/**
+ *
+ */
