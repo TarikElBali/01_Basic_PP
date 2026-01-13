5db4373f0dcf867580bf9cfd2bd79776ff723156
TarikElBali <tarik_elbali@hotmail.de>
2026-01-13 11:03:44 +0000

    06_controlflow.js

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -23,19 +23,22 @@ const birthYearMark = 1990;
 
 
 // Berechnung Alter - Iteration 3
-let date = new Date().getFullYear();
+let year = new Date().getFullYear();
 console.log("Jahr: " + year);
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
-
-
-
-
-
-
-
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
+
+// Deklaration | Tests | Aussagenlogik
+// let isJohnOlder = false;
+let isJohnOlder = (ageJohn > ageMark); // Test auf Ungleichheit
+let isJohnEqual = (ageJohn == ageMark) // Test auf Gleichheit
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+
+
+
 
