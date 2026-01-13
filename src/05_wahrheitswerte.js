d7c9028ff92d2bd22a43348d2f48ab6724e51f76
TarikElBali <tarik_elbali@hotmail.de>
2026-01-13 09:51:04 +0000

    05_wahrheitswerte.js Iteration 1-3

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,10 +6,34 @@ let ageJohn, ageMark;
 const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2024;
+// // Berechnung Alter - Iteration 1
+// let year = 2026;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+
+// // Berechnung Alter - Iteration 2
+// let date = new Date();
+// console.log("Datum: " + date)
+// let year = date.getFullYear();
+// console.log("Jahr: " + year);
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+
+
+// Berechnung Alter - Iteration 3
+let date = new Date().getFullYear();
+console.log("Jahr: " + year);
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
+
+
+
+
+
+
+
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
