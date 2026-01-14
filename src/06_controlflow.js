93953f21ecc526399f52d8a17d7a5f464e48b8d2
TarikElBali <tarik_elbali@hotmail.de>
2026-01-14 08:14:03 +0000

    06_controlflow.js if, if-else

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -12,9 +12,36 @@ isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
 // Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+console.log("------------------");
 
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)
+// if(false)
+// if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter.");
+// }
+
+ /************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+// if (true)
+// if(false)
+if(isJohnOlder)
+{
+    console.log("John ist älter.");
+}
+else
+{
+    console.log("John ist jünger.");
+}
