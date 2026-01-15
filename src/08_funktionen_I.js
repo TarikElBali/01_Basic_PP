27926887a309d59ee6410297ee7f9ac8e56ccadd
TarikElBali <tarik_elbali@hotmail.de>
2026-01-15 09:21:58 +0000

    08_Parametrisierung + Datenübergabe von INNEN

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -3,12 +3,28 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf | call
-test();
+// test();
 
  // Funktionsrumpf | (callee)
-// Funktionsdeklaratio
-function test()
-{
-console.log("Hallo, Simonas!");
+// Funktionsdeklaration
+// function test()
+// {
+//     console.log("Hallo, Simonas!");
+// }
+
+/***** Funktionen 02a *****/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// call
+ausgabeNamen();
+
+// Funktionalität
+function ausgabeNamen() {
+    // interne Variable | What happens in VEGAS
+    let firstName = "Yasin";
+
+    console.log("Hallo, " + firstName + "!");
+
 }
 
+// console.log(firstName); // Fehler Scope
