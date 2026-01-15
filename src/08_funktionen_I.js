8d54069faa093a541eff5aea6ba3a2cc200fb863
TarikElBali <tarik_elbali@hotmail.de>
2026-01-15 08:21:26 +0000

    08_funktionen_Kapselung_von_Codeblöcken

---
+++ src/08_funktionen_I.js
@@ -0,0 +1,14 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf | call
+test();
+
+ // Funktionsrumpf | (callee)
+// Funktionsdeklaratio
+function test()
+{
+console.log("Hallo, Simonas!");
+}
+
